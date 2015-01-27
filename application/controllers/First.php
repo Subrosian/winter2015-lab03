783a94294ffc72d36415869bdd75af7203dcda68
Jacob <subrosian@hotmail.com>
2015-01-27 13:05:10 -0800

    Some more lab 3 work. Signed-off-by:Jacob <subrosian@hotmail.com>

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -44,15 +44,10 @@ class First extends Application {
 
         $this->render();
     }
-    function gimme() {
-        $this->data['pagebody'] = 'homepage';    // this is the view we want shown
-        // build the list of authors, to pass on to our view
-        $source = $this->quotes->all();
-        $authors = array();
-        foreach ($source as $record) {
-            $authors[] = array('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
-        }
-        $this->data['authors'] = $authors;
+    function gimme($which) {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        $record = $this->quotes->get($which);
+        $this->data = array_merge($this->data, $record);
 
         $this->render();
     }
